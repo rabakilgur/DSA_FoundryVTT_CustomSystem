c1a571d518e31f5f981e87605c66fd40508c54df
Robin Garbe <mail@robin-garbe.de>
2021-03-07 19:00:28 +0100

    Fixed a bug

--- modules/hooks/init.js
+++ modules/hooks/init.js
@@ -46,6 +46,7 @@ Hooks.once("init", () => {
 	loadTemplates([
 		"systems/cDSA/templates/actors/actor-main.html",
 		"systems/cDSA/templates/actors/actor-talents.html",
+		"systems/cDSA/templates/actors/actor-talents-cDSA.html",
 		"systems/cDSA/templates/items/item-description.html",
 		"systems/cDSA/templates/dialog/default-dialog.html",
 		"systems/cDSA/templates/dialog/enhanced-default-dialog.html",
